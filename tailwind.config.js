b75478e768833fe94c80081fa5369d359dd19379
07Fahim10 <kingfahim7star143@gmail.com>
2024-11-06 20:14:35 +0600

    Tailwind Project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["*"],
+  theme: {
+    extend: {
+      "colors": {
+        'color-1': ' #5677AC',
+        'color-2': '#445F89',
+      }
+    },
+  },
+  plugins: [],
+}
